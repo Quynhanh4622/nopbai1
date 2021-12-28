c42b12c5228601ef897a023f9bd5e47c96397d01
Quynh anh <71160175+Quynhanh4622@users.noreply.github.com>
2021-12-28 14:01:31 +0700

    test

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="baiThi.MvcApplication" Language="C#" %>
---
+++ Models/baiThi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace baiThi.Models
+{
+    public class baiThi
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ExamSubject { get; set; }
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Time)]
+        public DateTime StartTime { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ExamDate { get; set; }
+        public int ExamDuration { get; set; }
+        public string ClassRoom { get; set; }
+        public string Faculty { get; set; }
+        public string Status { get; set; }
+    }
+}
